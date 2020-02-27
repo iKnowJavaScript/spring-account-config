13a8ec7bb52ef51b1f0e1e310ea7489dcd219f26
Omolayo Victor <vomolayo@seamfix.com>
2020-02-27 19:12:45 +0100

    Generate spring starter with mongo

---
+++ src/main/java/com/kingkonsole/accountconfig/AccountConfigApplication.java
@@ -0,0 +1,13 @@
+package com.kingkonsole.accountconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountConfigApplication.class, args);
+	}
+
+}
